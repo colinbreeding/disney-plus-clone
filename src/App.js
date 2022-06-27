59f9134ca63f6cbc83036cb087e7b8893d3a2a72
Colin Breeding <93558819+colinmb5@users.noreply.github.com>
2022-06-26 21:01:48 -0400

    setup navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 import "./App.css";
-import { Fragment } from "react";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
         </Routes>
